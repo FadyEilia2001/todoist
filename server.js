078a6f56a1098a720ee826b878ed424245a0c819
FadyEilia2001 <feilia@WGU.edu>
2022-11-09 20:41:34 -0600

    connected to DB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const connectDB = require("./db/connectDB");
 require("dotenv").config();
 
 const app = express();
@@ -9,4 +10,23 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//error middleware
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+//start server
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`DB Connected and server is listening on port ${port}!`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
