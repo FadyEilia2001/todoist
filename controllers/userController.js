fb304fe42231261a8016606f8858a02c90f04074
FadyEilia2001 <feilia@WGU.edu>
2022-11-10 10:52:03 -0600

    user model complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,9 @@
-const register = (req, res) => {
-  res.json({ msg: "create User Here" });
+const UserModel = require("../models/UserModel");
+
+const register = async (req, res) => {
+  const { firstName, email, password } = req.body;
+  const user = await UserModel.create(req.body);
+  res.json(user);
 };
 
 const login = (req, res) => {
