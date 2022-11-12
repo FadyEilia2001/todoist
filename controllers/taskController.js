045c1723e63a581389f2eb045b4708dc01ebe2c4
FadyEilia2001 <102275128+FadyEilia2001@users.noreply.github.com>
2022-11-12 13:30:49 -0600

    set up task router and controller structure

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,5 +1,25 @@
 const createNewTask = (req, res) => {
   res.json({ msg: "create a new task" });
 };
+const deleteTask = (req, res) => {
+  res.json({ msg: "Delete a task" });
+};
 
-module.exports = { createNewTask };
+const getAllTasks = (req, res) => {
+  res.json({ msg: "getAllTasks" });
+};
+const getSingleTask = (req, res) => {
+  res.json({ msg: "getSingleTask" });
+};
+
+const updateTask = (req, res) => {
+  res.json({ msg: "updateTask" });
+};
+
+module.exports = {
+  createNewTask,
+  deleteTask,
+  getAllTasks,
+  getSingleTask,
+  updateTask,
+};
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,7 +1,28 @@
-const { createNewTask } = require("../controllers/taskController");
+const {
+  createNewTask,
+  deleteTask,
+  getAllTasks,
+  getSingleTask,
+  updateTask,
+} = require("../controllers/taskController");
 
 const express = require("express");
 
 const router = express.Router();
+
+//create task
+router.post("/", createNewTask);
+
+//get all tasks
+router.get("/", getAllTasks);
+
+//get single task
+router.get("/:id", getSingleTask);
+
+//update task
+router.patch("/:id", updateTask);
+
+//delete task
+router.delete("/:id", deleteTask);
 
 module.exports = router;
