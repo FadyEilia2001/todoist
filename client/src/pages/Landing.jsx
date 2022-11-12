c14b6deedcb3c5cc324ff8628861bdeed841474f
FadyEilia2001 <feilia@WGU.edu>
2022-11-12 08:48:16 -0600

    fixed landing bugs

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -32,7 +32,7 @@ const Landing = () => {
         </Link>
       </main>
 
-      <section className="flex flex-col items-center p-4">
+      <section className="flex flex-col p-4">
         <div className="flex flex-col md:flex-row items-center justify-center gap-x-8 text-center md:text-start">
           <img src={clearYourMind} alt="tasks" className="w-1/2 mt-16 mb-8" />
           <div>
@@ -51,19 +51,20 @@ const Landing = () => {
           </div>
         </div>
 
-        <div className="container flex flex-col md:flex-row items-center justify-center mx-auto mt-12 mb-12">
-          <div className="flex flex-col items-center text-center md:text-left justify-center ">
+        <div className="flex flex-col md:flex-row-reverse items-center justify-center gap-x-8 text-center md:text-start">
+          <img src={getMoreDone} alt="tasks" className="w-1/2 mt-16 mb-8" />
+          <div>
             <p>Clear your mind</p>
             <h3 className="text-xl font-bold">
               Reach that mental clarity you’ve been longing for.
             </h3>
             <p>Your to-do lists are automatically sorted into Today,</p>
+
             <p>
               Upcoming and custom Filter views to help you focus on your most
               important things.
             </p>
           </div>
-          <img src={getMoreDone} className="w-1/2 mt-8" alt="" />
         </div>
       </section>
 
