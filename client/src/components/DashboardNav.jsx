d2cbbb5ef4d98e9fa8a5eead8c9a099b32386200
FadyEilia2001 <feilia@WGU.edu>
2022-11-12 22:48:50 -0600

    dashboard styles

--- client/src/components/DashboardNav.jsx
+++ client/src/components/DashboardNav.jsx
@@ -2,11 +2,12 @@ import {
   HomeOutlined,
   MenuOutlined,
   PlusCircleOutlined,
-  SearchOutlined,
 } from "@ant-design/icons";
-import { Avatar, Image } from "antd";
+import { Avatar } from "antd";
+import { useAppContext } from "../context/appContext";
 
 const DashboardNav = () => {
+  const { user } = useAppContext();
   return (
     <div className="h-12 p-4 flex items-center justify-between ">
       <div className="flex items-center w-28 justify-between mt-2">
@@ -16,8 +17,16 @@ const DashboardNav = () => {
 
       <div className="flex w-28 items-center justify-between">
         <PlusCircleOutlined className="text-3xl mt-2 text-white cursor-pointer " />
-        <Avatar style={{ color: "#f56a00", backgroundColor: "#fde3cf" }}>
-          U
+        <Avatar
+          style={{
+            color: "white",
+            backgroundColor: "#E73535",
+            boxShadow: "2px 0px 2px 0px rgba(0,0,0,0.75)",
+            border: "2.5px solid white",
+            fontSize:'1.2rem'
+          }}
+        >
+          {user.firstName.charAt(0).toUpperCase()}
         </Avatar>
       </div>
     </div>
